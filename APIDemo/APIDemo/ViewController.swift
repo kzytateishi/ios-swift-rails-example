cea3f9b9c9a847273fec4a8ccca87a0dba543d43
Kazuya Tateishi <kzy52m@gmail.com>
2015-03-25 21:40:07 +0900

    ios first commit

---
+++ APIDemo/APIDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APIDemo
+//
+//  Created by Kazuya Tateishi on 2015/03/25.
+//  Copyright (c) 2015年 kzy52. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
